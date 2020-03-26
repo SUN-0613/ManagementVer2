a463bdabf188b046bdb04b7c0ef6fd5d4990c5d0
AYamamoto <a-yamamoto@rss-dev.co.jp>
2020-03-26 10:23:18 +0900

    プロジェクト作成

---
+++ Management/App.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System.Windows;
+
+namespace Management
+{
+    /// <summary>
+    /// App.xaml の相互作用ロジック
+    /// </summary>
+    public partial class App : Application
+    {
+
+        /// <summary>処理開始</summary>
+        /// <param name="e">開始イベントデータ</param>
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+        }
+
+    }
+
+}
